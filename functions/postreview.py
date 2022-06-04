f31fe7867e60af5351fb51f8d8c7fb9ac48b8802
intelligence100 <95929354+intelligence100@users.noreply.github.com>
2022-06-04 17:10:26 +0530

    Update postreview.py

--- functions/postreview.py
+++ functions/postreview.py
@@ -1,5 +1,5 @@
 # IBM Action, method=GET, Python 3.9
-# API https://4ea3b251.us-south.apigw.appdomain.cloud/postreview
+# API https://eca74085.eu-gb.apigw.appdomain.cloud/postreviews
 # Params
 # {
 # "review": 
